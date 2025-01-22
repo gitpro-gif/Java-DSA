6a78a76d1f6e249919df67accb614f1970b31c18
gitpro-gif <vedantgithub2@gmail.com>
2025-01-22 14:01:49 +0530

    Print all Subarrays

---
+++ Array_det/Sub.java
@@ -0,0 +1,24 @@
+package Array_det;
+
+public class Sub {
+    public static void printSubArray(int numbers[]) {
+        int tp = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for(int j = i; j < numbers.length; j++) {
+                int end = j;
+                for(int k = start; k <= end; k++) {
+                    System.out.print(numbers[k] + " "); //subarray
+                }
+                tp++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("total Subarray =" + tp);
+    }
+    public static void main(String args[]) {
+        int numbers[] = {2,4,6,8,10};
+        printSubArray(numbers);
+    }
+}
