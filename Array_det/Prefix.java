786992ffc395e50308ad3ff25ce0bf4db4cdc16c
gitpro-gif <vedantgithub2@gmail.com>
2025-01-22 17:02:03 +0530

    Most optimized Kadanes code for sum of subarray

--- Array_det/Prefix.java
+++ Array_det/Prefix.java
@@ -28,8 +28,23 @@ public class Prefix {
         }
         System.out.println("max sum = " + maxSum);
     }
+
+    public static void Kadanes(int numbers[]) {
+        int max = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i =0; i < numbers.length; i++) {
+            cs = cs + numbers[i];
+            if(cs < 0) {
+                cs = 0;
+            }
+            max = Math.max(cs, max);
+        }
+        System.out.println("Our max subarray sum is : " + max);
+    }
+
     public static void main(String args[]) {
-        int numbers[] = {1,-2,6,-1,3};
-        MaxSubArray(numbers);
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        Kadanes(numbers);
     }
 }
